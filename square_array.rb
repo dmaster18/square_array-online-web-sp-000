0c5fbf529e58a81436a32e97e067c51ff00f40f7
David Strauch <dmaster18@gmail.com>
2019-07-24 14:24:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,7 +5,6 @@ def square_array(array)
     squared_elements.pop(squared_element)
   end
   return squared_elements
-  puts "This is the squared array: #{squared_elements}"
 end
 
 square_array([1, 2, 3, 4, 5])
