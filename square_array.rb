5d59de453c304f04257fd5392befe44ebbebab43
David Strauch <dmaster18@gmail.com>
2019-07-24 14:02:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  new_array
+  new_array = []
   array.each do |array_element|
     squared_element = array_element*array_element
-    new_array<<
+    new_array<<squared_
   end
   # your code here
 end
