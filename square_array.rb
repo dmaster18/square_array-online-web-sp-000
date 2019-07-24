a8bba3ff2dea8395c968493cf7681a92cc81b164
David Strauch <dmaster18@gmail.com>
2019-07-24 14:21:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
     squared_element = array_element*array_element
     squared_elements.pop(squared_element)
   end
-  puts "This is the squared array: #{array}"
+  puts "This is the squared array: #{squared_elements}"
 end
 
 square_array([1, 2, 3, 4, 5])
