b2f6d4b4363f554408f367d6e41f383e96bcdb65
David Strauch <dmaster18@gmail.com>
2019-07-24 14:22:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
+    squared_elements = []
 def square_array(array)
   array.each do |array_element|
-    squared_elements = []
+
     squared_element = array_element*array_element
     squared_elements.pop(squared_element)
   end
